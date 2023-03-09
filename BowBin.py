aed00898f7e405347c6f10639017146b206fd77e
GagliardeNicolapio <47244898+GagliardeNicolapio@users.noreply.github.com>
2023-03-09 12:33:31 +0100

    Add files via upload

--- BowBin.py
+++ BowBin.py
@@ -28,57 +28,57 @@ print_header()
 
 def debug(msg:str):
   if 'DEBUG_FLAG' in globals():
-    print(bcolors.WARNING+"DEBUG: "+msg+bcolors.ENDC)
+    print(bcolors.WARNING+"DEBUG: ")
+    print(msg)
+    print(bcolors.ENDC)
 
 def check_return(result: subprocess.CompletedProcess, error_msg:str):
-  print(result)
+  debug(result)
   if (result.returncode != 0 or len(result.stderr)>0):
       print(error_msg)
       exit(-1)
 
 #trim_galore uses stderr for print in shell
-def check_return_trim_galore(result: subprocess.CompletedProcess, error_msg:str):
-  print(result)
+def check_returncode(result: subprocess.CompletedProcess, error_msg:str):
+  debug(result)
   if (result.returncode != 0):
       print(error_msg)
       exit(-1)
 
 def check_file_exist(file_path:str):
   if not Path(file_path).is_file():
-      print("The file "+ file_path +" does not exist")
+      print(bcolors.FAIL+"The file "+ file_path +" does not exist"+bcolors.ENDC)
       exit(-1)
 
 def msg_usage(name=None):                                                            
   return '''BowBin.py [options]* (-in_reads FASTQ | -dein_reads FASTQ1 FASTQ2 | -sraid SRAID)'''
 
 def trim_galore(file1:str, file2:str):
-  debug("file1: "+file1)
-  debug("file2: "+file2)
+  debug("start trim galore")
   adapter_quality_trim_command = "trim_galore --paired "+file1+" "+file2+" -o "+OUTPUT_FOLDER
- 
   debug(adapter_quality_trim_command)
-  check_return_trim_galore(subprocess.run(adapter_quality_trim_command, capture_output=True, shell=True),"adapter removal or quality trimming error")
-
+  check_returncode(subprocess.run(adapter_quality_trim_command, capture_output=True, shell=True),"adapter removal or quality trimming error")
+  return file1.replace(".fq","_val_1.fq"), file2.replace(".fq","_val_2.fq")
 
 def trim_galore_polya(file1:str, file2:str):
-    debug("file1: "+file1)
-    debug("file2: "+file2)
+    debug("start trim galore polyA")
     #controllare virusseeker e genome detective dove fanno la polyA
     #provare senza polyA
     #polya e' sperimentale, il flag -o non funziona, quindi dopo polya faccio mv dei file
-    polyA_command = "trim_galore --polyA --paired "+file1.replace(".fq","_val_1.fq")+" "+file2.replace(".fq","_val_2.fq")
+    polyA_command = "trim_galore --polyA --paired "+file1+" "+file2
     debug(polyA_command)
-    check_return_trim_galore(subprocess.run(polyA_command, capture_output=True, shell=True),"polyA error")
+    check_returncode(subprocess.run(polyA_command, capture_output=True, shell=True),"polyA error")
 
-    debug(file1.replace(".fq","_val_1_val_1.fq"))
-    
-    move_files = "mv ./"+file1.replace(".fq","_val_1_val_1.fq").replace(OUTPUT_FOLDER,"")+" ./"+file2.replace(".fq","_val_2_val_2.fq").replace(OUTPUT_FOLDER,"")+" "+OUTPUT_FOLDER
+
+    file1 = file1.replace(OUTPUT_FOLDER,"").replace(".fq","_val_1.fq")
+    file2 = file2.replace(OUTPUT_FOLDER,"").replace(".fq","_val_2.fq")
+    move_files = "mv ./"+file1+" ./"+file2+" "+OUTPUT_FOLDER
     debug(move_files)
     check_return(subprocess.run(move_files, capture_output=True, shell=True),"mv error")
+    return OUTPUT_FOLDER+file1, OUTPUT_FOLDER+file2
 
 def fastq_join(file1:str, file2:str):
-  debug("file1: "+file1)
-  debug("file2: "+file2)
+  debug("fastq-join start")
 
   fastq_join_command = "fastq-join "+file1.replace(".fq","_val_1_val_1.fq")+" "+file2.replace(".fq","_val_2_val_2.fq")+" -o "+OUTPUT_FOLDER+"result-fq"
   debug(fastq_join_command)
@@ -91,6 +91,33 @@ def cd_hit():#problema: ci mette un eternità di tempo
   check_return(subprocess.run(cd_hit_command, capture_output=True, shell=True), "cd-hit error")
 
 
+#allineamento
+# bowtie2 -x indice -1 reads-1_val_1_val_1.fq -2 reads-2_val_2_val_2.fq -S risultati.sam
+
+def bowtie2_alignment(file1:str, file2:str):
+  debug("bowtie2 start")
+  bowtie2_command = "bowtie2 -x  virusTrovatiInERR13GenomDetect/indice -1 "+file1+" -2 "+file2+" -S "+OUTPUT_FOLDER+"results.sam"  
+  debug(bowtie2_command)
+  check_returncode(subprocess.run(bowtie2_command, capture_output=True, shell=True), "bowtie2 alignment error")
+  
+
+def samtools():
+  debug("samtools start")
+  view_command = "samtools view -S -b "+OUTPUT_FOLDER+"results.sam > "+OUTPUT_FOLDER+"results.bam"
+  debug(view_command)
+  check_return(subprocess.run(view_command, capture_output=True, shell=True),"samtools view error")
+
+  sort_command = "samtools sort "+OUTPUT_FOLDER+"results.bam > "+OUTPUT_FOLDER+"results.sort.bam"
+  debug(sort_command)
+  check_return(subprocess.run(sort_command, capture_output=True, shell=True),"satools sort error")
+
+#conda install -c bioconda qualimap
+def quali_map():
+  debug("start quali map")
+  quali_map_command = "qualimap bamqc -bam "+OUTPUT_FOLDER+"results.sort.bam -outdir "+OUTPUT_FOLDER+"/report_results"
+  debug(quali_map_command)
+  check_return(subprocess.run(quali_map_command, capture_output=True, shell=True),"quali map error")
+
 # Gather our code in a main() function
 def main(args):
 
@@ -101,18 +128,22 @@ def main(args):
 
     #adapter removale, quality trimming, polyA
     debug("start trim galore"+args.dein_reads[0]+"  "+args.dein_reads[1])
-    trim_galore(args.dein_reads[0],args.dein_reads[1])
+    trimmed_file_1, trimmed_file_2 = trim_galore(args.dein_reads[0],args.dein_reads[1])
 
 
     #polyA
-    trim_galore_polya(args.dein_reads[0],args.dein_reads[1])
+    trim_poly_file_1, trim_poly_file_2 = trim_galore_polya(trimmed_file_1,trimmed_file_2)
     
     #join overlapping reads
-    fastq_join(args.dein_reads[0],args.dein_reads[1])
+    #fastq_join(args.dein_reads[0],args.dein_reads[1])
 
     #cd-hit clustering reads
-    cd_hit()
+    #cd_hit()
 
+
+
+
+#python BowBin/BowBin/BowBin.py -in_reads datiMetagen/ERR233413.fastq -o virusERR13senzaFastjoin/ -debug
   elif args.in_reads is not None:
     check_file_exist(args.in_reads)
     #fare check estensione (?)
@@ -130,17 +161,25 @@ def main(args):
 
     #adapter removale, quality trimming, polyA
     debug("start trim_galore "+OUTPUT_FOLDER+"reads-1.fq  "+OUTPUT_FOLDER+"reads-2.fq")
-    trim_galore(OUTPUT_FOLDER+"reads-1.fq",OUTPUT_FOLDER+"reads-2.fq")
+    trimmed_file_1, trimmed_file_2 = trim_galore(OUTPUT_FOLDER+"reads-1.fq",OUTPUT_FOLDER+"reads-2.fq")
   
 
     #polya
-    trim_galore_polya(OUTPUT_FOLDER+"reads-1.fq",OUTPUT_FOLDER+"reads-2.fq")
+    trim_poly_file_1, trim_poly_file_2 = trim_galore_polya(trimmed_file_1, trimmed_file_2)
 
     #join overlapping reads
-    fastq_join(OUTPUT_FOLDER+"reads-1.fq",OUTPUT_FOLDER+"reads-2.fq")
+    #fastq_join(OUTPUT_FOLDER+"reads-1.fq",OUTPUT_FOLDER+"reads-2.fq")
 
     #cd-hit clustering reads
-    cd_hit()
+    #cd_hit()
+
+    #bowtie2 alignment
+    bowtie2_alignment(trim_poly_file_1, trim_poly_file_2)
+
+    #samtools view and sort
+    samtools()
+
+    quali_map()
 
   else:
     print(3)
